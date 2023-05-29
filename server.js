49161e8692b88186e2e5122e2bdcd561cf194284
@JRC04 <joeljrc04@gmail.com>
2023-05-29 22:32:38 +0200

    Basic Server Side

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const keys = require('./config/keys.js');
-
 const app = express();
 
 // Setting up Database
@@ -9,47 +8,11 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true
 
 // Setup Database models
 require('./model/Account');
-const Account = mongoose.model('accounts');
 
-// Routes
-app.get('/account', async (req, res) => {
-    
-    const { rUsername, rPassword } = req.query;
-    if(rUsername == null || rPassword == null) {
-        res.send("Invalid Credentials");
-        return;
-    }
+// stack more models and routes
 
-    var userAccount = await Account.findOne({ username: rUsername});
-    if(userAccount == null) {
-        // Create new account
-        console.log("Create new account...");
-        
-        var newAccount = new Account({
-            username : rUsername,
-            password : rPassword,
-
-            lastAuthentication : Date.now(),
-        });
-        await newAccount.save();
-        
-        res.send(newAccount);
-        return;
-    }
-    else {
-        if(rPassword == userAccount.password) {
-            userAccount.lastAuthentication = Date.now();
-            await userAccount.save();
-
-            console.log("Retrieving account...");
-            res.send(userAccount);
-            return;
-        }
-    }
-
-    res.send("Invalid Credentials");
-    return;
-});
+// Setup the routes
+require('./routes/authenticationRoute')(app);
 
 
 app.listen(keys.port, () => {
